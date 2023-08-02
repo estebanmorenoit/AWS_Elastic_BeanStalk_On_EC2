602f03b18aab3a661103decb619e5968c3cc20f1
estebanmorenoit <98739538+estebanmorenoit@users.noreply.github.com>
2023-08-02 15:35:17 +0100

    App.js update

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const customText = "Welcome to My Awesome React App!"; // Customized text added
+  const learnMoreLink = "https://reactjs.org"; // Customized learn more link
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {customText}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={learnMoreLink} // Replaced the original link with customized link
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More About React
         </a>
       </header>
     </div>
